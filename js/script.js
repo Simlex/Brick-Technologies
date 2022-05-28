42b0cfe8a3326362157fb0cb049c56b7e8cf61c3
Simlex <gettosimlex@gmail.com>
2022-05-28 17:06:41 +0100

    AOS, JS
    Added aos animations, updated js codes, updated styles and gave the navigation links transitions

--- js/script.js
+++ js/script.js
@@ -1,6 +1,39 @@
-$(function () {
+//$(function () {
 
-    const openNav = document.querySelector('.nav-menu');
+    const openNav = document.querySelector('.nav-open');
+    const closeNav = document.querySelector('.nav-close');
+    const overlay = document.querySelector('.nav-overlay');
+    const navLinks = document.querySelector('.nav-links');
 
     
-});
+    const openNavigation = () => {
+        if(overlay.style.display = "none") {
+            overlay.style.display = ""
+        }
+        openNav.style.display = "none";
+        closeNav.style.display = "block";
+    }
+    
+    const closeNavigation = () => {
+        if(overlay.style.display = "flex") {
+            overlay.style.display = "none"
+        }
+        closeNav.style.display = "none";
+        openNav.style.display = "";
+    }
+    
+    openNav.addEventListener('click', () => {
+        console.log('Nav-manu clicked');
+    });
+
+    openNav.addEventListener('click', () => {
+        /* navLinks.setAttribute('data-aos', 'fade-up'); */
+        openNavigation();
+    });
+
+    //openNav.addEventListener('click', () => { openNavigation() });
+    closeNav.addEventListener('click', () => { 
+        
+        closeNavigation() 
+    });
+//});
